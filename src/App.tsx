25654444934d19069c5eb2576dfd1207b3a62f35
Laerte Quinui <laertequinuio@gmail.com>
2025-09-03 09:53:53 -0300

    feat: Configure theme colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import { ConfigProvider } from 'antd'
+
 function App() {
   return (
-    <>
+    <ConfigProvider
+      theme={{
+        cssVar: true,
+        token: {
+          colorPrimary: '#00D084',
+          colorSuccess: '#FF6900',
+          colorText: '#172121',
+          colorBgContainer: '#FFFFFF'
+        }
+      }}
+    >
       <h1>Hello world</h1>
-    </>
+    </ConfigProvider>
   )
 }
 
