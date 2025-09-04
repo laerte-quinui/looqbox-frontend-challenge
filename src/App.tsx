31ecc8bc1e7c0f71014c054862f25bc55e2ec926
Laerte Quinui <laertequinuio@gmail.com>
2025-09-04 09:08:19 -0300

    feat: Integrate React-Query for data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ConfigProvider, Layout } from 'antd'
 import Home from './pages/home/Home'
 
+const queryClient = new QueryClient()
+
 function App() {
   return (
-    <ConfigProvider
-      theme={{
-        cssVar: true,
-        token: {
-          colorPrimary: '#00D084',
-          colorSuccess: '#FF6900',
-          colorText: '#172121',
-          colorBgContainer: '#FFFFFF'
-        }
-      }}
-    >
-      <Layout className="container">
-        <Home />
-      </Layout>
-    </ConfigProvider>
+    <QueryClientProvider client={queryClient}>
+      <ConfigProvider
+        theme={{
+          cssVar: true,
+          token: {
+            colorPrimary: '#00D084',
+            colorSuccess: '#FF6900',
+            colorText: '#172121',
+            colorBgContainer: '#FFFFFF'
+          }
+        }}
+      >
+        <Layout className="container">
+          <Home />
+        </Layout>
+      </ConfigProvider>
+    </QueryClientProvider>
   )
 }
 
