c6059108f17f279fb23a95caf6b15cfdcbb9bfb5
Laerte Quinui <laertequinuio@gmail.com>
2025-09-05 14:12:41 -0300

    feat: Integrate react-router for navigation

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,42 @@
+import { StyleProvider } from '@ant-design/cssinjs'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ConfigProvider, Layout } from 'antd'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter } from 'react-router'
+import { RouterProvider } from 'react-router/dom'
+
 import './index.css'
-import App from './App.tsx'
+import Home from './pages/home/Home'
+
+const queryClient = new QueryClient()
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <QueryClientProvider client={queryClient}>
+      <StyleProvider layer>
+        <ConfigProvider
+          theme={{
+            cssVar: true,
+            token: {
+              colorPrimary: '#00D084',
+              colorSuccess: '#FF6900',
+              colorText: '#172121',
+              colorBgContainer: '#FFFFFF'
+            }
+          }}
+        >
+          <Layout className="container">
+            <RouterProvider router={router} />
+          </Layout>
+        </ConfigProvider>
+      </StyleProvider>
+    </QueryClientProvider>
+  </StrictMode>
 )
