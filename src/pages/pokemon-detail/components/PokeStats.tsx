e98c486684944474fd8064a7d252cb37bcbc41fc
Laerte Quinui <laertequinuio@gmail.com>
2025-09-07 17:43:43 -0300

    feat: Add Recharts and create DonutChart component

--- src/pages/pokemon-detail/components/PokeStats.tsx
+++ src/pages/pokemon-detail/components/PokeStats.tsx
@@ -1,10 +1,20 @@
 import { Col, Flex, Row, Statistic } from 'antd'
+import DonutChart from '../../../components/DonutChart'
 
 const PokeStats = () => {
   const measurements = [
     { title: 'Weight', value: 27.8, suffix: 'kg' },
     { title: 'Height', value: 153.3, suffix: 'cm' }
   ]
+  const stats = [
+    { name: 'Base Health', value: 45 },
+    { name: 'Base Attack', value: 49 },
+    { name: 'Base Defense', value: 49 },
+    { name: 'Base Speed', value: 69 },
+    { name: 'Special Attack', value: 65 },
+    { name: 'Special Defense', value: 65 }
+  ]
+
   return (
     <Row gutter={24} style={{ height: '100%' }}>
       {/* Measurements */}
@@ -15,7 +25,17 @@ const PokeStats = () => {
           ))}
         </Flex>
       </Col>
-      <Col span={12}></Col>
+
+      {/* Stats */}
+      <Col span={12}>
+        <Row style={{ height: '100%' }}>
+          {stats.map(stat => (
+            <Col span={8} key={stat.name}>
+              <DonutChart data={stat} />
+            </Col>
+          ))}
+        </Row>
+      </Col>
     </Row>
   )
 }
